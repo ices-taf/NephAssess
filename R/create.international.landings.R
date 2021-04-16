ffdd5fd7830e487bf81041bc9ef73de3ea7589ac
C-Mesquita <39331069+C-Mesquita@users.noreply.github.com>
2021-04-16 10:53:42 +0100

    change in create.international.landings.R
    Changed create.international.landings.R to account or new format of “international.landings.csv” file which now splits the landings in “Scotland”, “Other countries” and “Total”. Also, the time-series of Scottish landings in the output file “international.landings.csv” is updated with the values included in the nephup data object.

--- R/create.international.landings.R
+++ R/create.international.landings.R
@@ -8,14 +8,17 @@ create.international.landings<- function(wdir, fu, new.data.year, old.int.landin
   if (!fu==unique(ic$Stock)) { stop("FU in Intercatch file does not match specified FU") }
   if (!new.data.year==unique(ic$Year)) { stop("Year in Intercatch file does not match new.data.year") }
   
-  official.Scotland<- scot[scot$Year==new.data.year,"Sub.Total"]
+  official.Scotland.new.year<- scot[scot$Year==new.data.year,"Sub.Total"]
   land.by.country<- as.data.frame((as.table(round(tapply(ic.landings$Catch..kg, list(ic.landings$Country), sum)/1000))))
 
   names(land.by.country)<- c("Country", "LANDINGS_IC")
   land.by.country$LANDINGS_OFFICIAL<- land.by.country$LANDINGS
-  land.by.country[land.by.country$Country %in% "UK(Scotland)","LANDINGS_OFFICIAL"]<- official.Scotland
+  land.by.country[land.by.country$Country %in% "UK(Scotland)","LANDINGS_OFFICIAL"]<- official.Scotland.new.year
   write.csv(land.by.country, file=paste0(wdir,"landings.by.country.csv"), row.names = FALSE)
   
-  int[nrow(int)+1,] <- c(new.data.year,sum(land.by.country$LANDINGS_OFFICIAL))
+  common.years<- intersect(int$Year,scot$Year)
+  int[int$Year %in% common.years, "Scotland"]<- scot[scot$Year %in% common.years, "Sub.Total"]
+  int$Total<- int$Scotland + int$Other.Countries
+  int[nrow(int)+1,] <- c(new.data.year,land.by.country[land.by.country$Country %in% "UK(Scotland)", "LANDINGS_OFFICIAL"], sum(land.by.country[!land.by.country$Country %in% "UK(Scotland)", "LANDINGS_OFFICIAL"]), sum(land.by.country$LANDINGS_OFFICIAL))
   write.csv(int, file=paste0(wdir,"international.landings.csv"), row.names = FALSE)
 }
